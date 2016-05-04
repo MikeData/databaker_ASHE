43e43dca219dbe629a65c07670f7f763a7e3578a
Michael Adams <mikelovesbooks@gmail.com>
2016-05-04 14:14:43 +0100

    Update ASHE_merger.py

--- ASHE_merger.py
+++ ASHE_merger.py
@@ -20,7 +20,7 @@ def generic_merge(obs_file, which_ASHE):
     obs_file = obs_file.reset_index() 
     obs_file = obs_file.drop('index', axis=1)
 
-    # Correctthe obs count
+    # Correct the obs count
     obs_file['data_marking'][-1:] = len(obs_file) -1
 
     if which_ASHE >1 and which_ASHE < 7:
@@ -39,6 +39,7 @@ def merge_earnings(which_ASHE, fdone):
     
     import pandas as pd
     
+    # load all sheets. Strip the footer row of all except the last one
     a1 = pd.read_csv('merge_file_1.csv', dtype=object)
     a1 = a1[a1['observation'] != '*********']
     
@@ -74,6 +75,7 @@ def merge_hours(which_ASHE, fdone):
     
     import pandas as pd
     
+    # load all sheets. Strip the footer row of all except the last one
     a1 = pd.read_csv('merge_file_9.csv', dtype=object)
     a1 = a1[a1['observation'] != '*********']
     
@@ -85,7 +87,7 @@ def merge_hours(which_ASHE, fdone):
     obs_file = pd.concat([a1, a2, a3])
     obs_file = generic_merge(obs_file, which_ASHE)
     
-    # Switich "Earnings" Dim id to "Hours - THIS CODE IS NOT REQUIRED FOR EARNINGS
+    # Switich "Earnings" Dim id to "Hours" - THIS CODE IS NOT REQUIRED FOR EARNINGS
     obs_file['dim_id_1'][:-1] = 'Hours'
     obs_file['dimension_label_eng_1'] = obs_file['dim_id_1']
     # Switch "Earnings Statistics" to "Hours Statistics"
@@ -95,17 +97,19 @@ def merge_hours(which_ASHE, fdone):
     # Output final product
     obs_file.to_csv('transform-ASHE' + str(which_ASHE)+ 'hours' + fdone[0][-8:-4] + '.csv', index=False)
     
+    
+    
+    
 """  
 Manual running code
 Re-creates the automatic inputs and lets someone skip straight to merging the final files
 """
-
 import sys
 
 # Which table
 which_ASHE = sys.argv[1]
 
-# Adds a load of filler to year and puts it in a list, so the script can execute as per standard
+# Adds a load of filler to year and puts it in a list, so the script can execute wihout conditionals
 fdone = []
 fdone.append(sys.argv[2] + '####')
 
